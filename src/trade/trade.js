0689c39a5072e2a7cb36867c932ac6520386f4e0
muhammadali1995 <4377433mtDEV>
2022-01-07 14:10:42 +0500

    local changes

--- src/trade/trade.js
+++ src/trade/trade.js
@@ -7,9 +7,8 @@ const bottomModal = document.getElementById('bottom-modal')
 const originalHeight = element.offsetHeight;
 
 async function scrollFunction() {
-    console.log(document.body.scrollTop)
-    if (document.documentElement.scrollTop == 0) return;
-    console.log('still')
+    // console.log(document.body.scrollTop)
+    // if (document.documentElement.scrollTop == 0) return;
     if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
         const documentScrollTop = document.documentElement.scrollTop;
         const newHeight = (originalHeight - documentScrollTop + 40) > 100 ? originalHeight - documentScrollTop : 100;
@@ -19,7 +18,7 @@ async function scrollFunction() {
     } else {
         bottomModal.style.maxHeight = 'initial';
         bottomModal.style.overflow = 'initial';
-        document.getElementById('degree').style.height = 'auto';
+        document.getElementById('degree').style.height = '100%';
     }
 }
 window.onscroll = function () { scrollFunction() };
