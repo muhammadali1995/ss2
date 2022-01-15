c3d4f9768517f8ba2872371fd3abc4dfe55b125c
unknown <diyorbekabdumannonov4@gmail.com>
2022-01-15 17:15:31 +0500

    finished profile page

--- src/pages/portfolio/portfolio.js
+++ src/pages/portfolio/portfolio.js
@@ -1,15 +1,24 @@
 const PORTFOLIO_PAGE = 'portfolio'
 const FEED_PAGE = 'feedPage'
+const PROFILE_PAGE = 'profile'
 
 const portfolioNextBtn = document.querySelectorAll('.portfolioNextBtn')
-const feedNextBtn = document.querySelector('.feedNextBtn')
+const feedNextBtn = document.querySelectorAll('.feedNextBtn')
+const profileNextBtns = document.querySelectorAll('.profileNextBtn')
 portfolioNextBtn.forEach(element => {
     element.addEventListener('click', () => {
         changePage(PORTFOLIO_PAGE)
     })
 })
-feedNextBtn.addEventListener('click', () => {
-    changePage(FEED_PAGE)
+feedNextBtn.forEach(element => {
+    element.addEventListener('click', () => {
+        changePage(FEED_PAGE)
+    })
+})
+profileNextBtns.forEach(element => {
+    element.addEventListener('click', () => {
+        changePage(PROFILE_PAGE)
+    })
 })
 
 function changePage(page) {
@@ -56,4 +65,53 @@ const upScroll = document.querySelector('#upScroll')
 document.addEventListener('scroll', () => {
     upScroll.style.minHeight = '375px'
     upScroll.style.maxHeight = '375x'
+})
+
+// PROFILE
+const profileMainPage = document.getElementById('mainPage')
+const abonentsPage = document.getElementById('abonents')
+const abonentNextBtn = document.getElementById('abonentNextBtn')
+const abonentPrevBtn = document.getElementById('abonentPrevBtn')
+
+abonentNextBtn.addEventListener('click', () => {
+    profileMainPage.classList.add('hidden')
+    abonentsPage.classList.remove('hidden')
+})
+
+abonentPrevBtn.addEventListener('click', () => {
+    profileMainPage.classList.remove('hidden')
+    abonentsPage.classList.add('hidden')
+})
+
+const followBtns = document.querySelectorAll('.follow')
+followBtns.forEach(followBtn => {
+    followBtn.addEventListener('click', () => {
+        if (followBtn.innerText == 'Se désabonner') {
+            followBtn.innerText = 'S’abonner'
+        } else {
+            followBtn.innerText = 'Se désabonner'
+        }
+    })
+})
+
+const personalInfInputs = document.querySelectorAll('.personalInfInput')
+personalInfInputs.forEach(personalInfInput => {
+    personalInfInput.addEventListener('focus', () => {
+        personalInfInput.classList.add('border-blue-normal', 'text-blue-normal')
+        personalInfInput.style.borderColor = 'red!important'
+    })
+})
+
+const personalInfBtn = document.getElementById('personalInfBtn')
+const personalInformationPage = document.getElementById('personalInformation')
+const personalPrevBtn = document.getElementById('personalPrevBtn')
+
+personalInfBtn.addEventListener('click', () => {
+    profileMainPage.classList.add('hidden')
+    personalInformationPage.classList.remove('hidden')
+})
+
+personalPrevBtn.addEventListener('click', () => {
+    profileMainPage.classList.remove('hidden')
+    personalInformationPage.classList.add('hidden')
 })
