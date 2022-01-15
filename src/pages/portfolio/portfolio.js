f695213e91c231cfb2fe037f6fe85251d9793067
unknown <diyorbekabdumannonov4@gmail.com>
2022-01-15 14:26:16 +0500

    added profile page

--- src/pages/portfolio/portfolio.js
+++ src/pages/portfolio/portfolio.js
@@ -1,3 +1,35 @@
+const PORTFOLIO_PAGE = 'portfolio'
+const FEED_PAGE = 'feedPage'
+
+const portfolioNextBtn = document.querySelectorAll('.portfolioNextBtn')
+const feedNextBtn = document.querySelector('.feedNextBtn')
+portfolioNextBtn.forEach(element => {
+    element.addEventListener('click', () => {
+        changePage(PORTFOLIO_PAGE)
+    })
+})
+feedNextBtn.addEventListener('click', () => {
+    changePage(FEED_PAGE)
+})
+
+function changePage(page) {
+    hidePages();
+    showPage(page)
+}
+
+
+function hidePages() {
+    const pages = document.querySelectorAll('.ss-page');
+    Array.from(pages).forEach(element => {
+        if (!element.classList.contains('hidden')) element.classList.add('hidden')
+    });
+}
+
+function showPage(pageId) {
+    const currPage = document.getElementById(pageId);
+    currPage.classList.remove('hidden');
+}
+
 const tousTab = document.getElementById('tousTab')
 const tousBtn = document.getElementById('tousBtn')
 const attenteTab = document.getElementById('attenteTab')
@@ -23,5 +55,5 @@ attenteBtn.addEventListener('click', () => {
 const upScroll = document.querySelector('#upScroll')
 document.addEventListener('scroll', () => {
     upScroll.style.minHeight = '375px'
-    upScroll.style.maxHeight = '375px'
+    upScroll.style.maxHeight = '375x'
 })
